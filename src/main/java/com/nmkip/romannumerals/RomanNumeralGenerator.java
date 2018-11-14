44f5a203f3293b3bdb2f96d9a6666ddb8676d72e
nmkip <juan.ignacio848@gmail.com>
2018-11-13 23:27:26 -0300

    Roman for 5.

--- src/main/java/com/nmkip/romannumerals/RomanNumeralGenerator.java
+++ src/main/java/com/nmkip/romannumerals/RomanNumeralGenerator.java
@@ -5,6 +5,10 @@ public class RomanNumeralGenerator {
     public static String romanFor(int decimal) {
 
         String roman = "";
+        if(decimal == 5) {
+            return "V";
+        }
+
         for(int i = 0; i < decimal; i++) {
             roman += "I";
         }
