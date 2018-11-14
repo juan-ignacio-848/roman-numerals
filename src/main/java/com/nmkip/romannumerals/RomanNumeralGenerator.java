c41f0c7ff4854c2c9f99df16fee80dff6855180a
nmkip <juan.ignacio848@gmail.com>
2018-11-13 23:26:07 -0300

    Removed duplication

--- src/main/java/com/nmkip/romannumerals/RomanNumeralGenerator.java
+++ src/main/java/com/nmkip/romannumerals/RomanNumeralGenerator.java
@@ -4,14 +4,11 @@ public class RomanNumeralGenerator {
 
     public static String romanFor(int decimal) {
 
-        if(decimal == 3) {
-            return "III";
+        String roman = "";
+        for(int i = 0; i < decimal; i++) {
+            roman += "I";
         }
 
-        if(decimal == 2) {
-            return "II";
-        }
-
-        return "I";
+        return roman;
     }
 }
