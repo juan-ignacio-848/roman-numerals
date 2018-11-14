440e101d9d6bfd9625142ee4b17ccab4430c331f
nmkip <juan.ignacio848@gmail.com>
2018-11-13 23:29:52 -0300

    Roman for 10 and 11.

--- src/main/java/com/nmkip/romannumerals/RomanNumeralGenerator.java
+++ src/main/java/com/nmkip/romannumerals/RomanNumeralGenerator.java
@@ -5,6 +5,12 @@ public class RomanNumeralGenerator {
     public static String romanFor(int decimal) {
 
         String roman = "";
+
+        if(decimal >= 10) {
+            roman += "X";
+            decimal -= 10;
+        }
+
         if(decimal >= 5) {
             roman += "V";
             decimal -= 5;
