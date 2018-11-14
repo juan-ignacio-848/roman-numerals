c0b08283a97f975cb9d2d5c6c66649b51422c27d
nmkip <juan.ignacio848@gmail.com>
2018-11-13 23:24:48 -0300

    Roman for 3.

--- src/main/java/com/nmkip/romannumerals/RomanNumeralGenerator.java
+++ src/main/java/com/nmkip/romannumerals/RomanNumeralGenerator.java
@@ -3,6 +3,11 @@ package com.nmkip.romannumerals;
 public class RomanNumeralGenerator {
 
     public static String romanFor(int decimal) {
+
+        if(decimal == 3) {
+            return "III";
+        }
+
         if(decimal == 2) {
             return "II";
         }
