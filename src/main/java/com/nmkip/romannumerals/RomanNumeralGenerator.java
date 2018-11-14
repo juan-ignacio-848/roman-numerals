265ce412aec77bf9e89275fd9b4d063136ecbec9
nmkip <juan.ignacio848@gmail.com>
2018-11-13 23:44:52 -0300

    Added the rest of special numerals and some test cases.

--- src/main/java/com/nmkip/romannumerals/RomanNumeralGenerator.java
+++ src/main/java/com/nmkip/romannumerals/RomanNumeralGenerator.java
@@ -3,7 +3,6 @@ package com.nmkip.romannumerals;
 public class RomanNumeralGenerator {
 
     public static String romanFor(int decimal) {
-
         String roman = "";
 
         for(RomanArabic romanArabic : RomanArabic.values()) {
@@ -18,6 +17,14 @@ public class RomanNumeralGenerator {
 
     private enum RomanArabic {
 
+        THOUSAND("M", 1000),
+        NINE_HUNDRED("CM", 900),
+        FIVE_HUNDRED("D", 500),
+        FOUR_HUNDRED("CD", 400),
+        HUNDRED("C", 100),
+        NINETY("XC", 90),
+        FIFTY("L", 50),
+        FORTY("XL", 40),
         TEN("X", 10),
         NINE("IX", 9),
         FIVE("V", 5),
